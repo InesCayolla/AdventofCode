d20a3666eafc75c34049940bbb6ca89c66514019
inescayolla <ines.cayolla@gmail.com>
2022-12-01 18:59:56 +0000

    Day 1 - Solved

--- Day1/Day1.rb
+++ Day1/Day1.rb
@@ -1,1 +1,9 @@
 
+calories = File.read("input.txt")
+  .split("\n\n") .map do |line|
+    line.split("\n").map(&:to_i).sum
+  end
+  .sort
+
+puts part_1 = calories.last
+puts part_2 = calories.last(3).sum
