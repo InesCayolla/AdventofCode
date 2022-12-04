eee4b9636c5bb948c5526dd888fad391558aef17
inescayolla <ines.cayolla@gmail.com>
2022-12-04 23:33:47 +0000

    Day4 - Solved

---
+++ Day4/Day4.rb
@@ -0,0 +1,24 @@
+
+assignments = File.read('Day4.txt').split("\n")
+inclusive_count = 0
+overlap_count = 0
+
+def to_range(input_array)
+  Range.new(input_array[0], input_array[1])
+end
+
+assignments.each do |pairings|
+  one, two = pairings.split(',').map {|x| to_range(x.split('-').map(&:to_i)) }
+
+  inclusive_count += 1 if one.cover?(two) || two.cover?(one)
+
+  one.each do |x|
+    if two.to_a.include?(x)
+      overlap_count +=1
+      break
+    end
+  end
+end
+
+puts inclusive_count
+puts overlap_count
